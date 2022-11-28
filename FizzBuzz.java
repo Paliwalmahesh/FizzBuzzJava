0c69a052515fc3aaf309072fff4ec9e2d599b6b5
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-11-28 16:35:01 +0530

    checking number is multiple of 3

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,13 +1,24 @@
 
 public class FizzBuzz {
 	public String Print_Fizz_Buzz(int input_number) {
-		
-		if(input_number>100 || input_number<0) {
+		String result="";
+		if(input_number>100 || input_number<=0) {
 			return "-1";
 		}
+		else {
+			for(int loop_input_number=1;loop_input_number<=input_number;loop_input_number++) {
+				if(loop_input_number%3==0) {
+					result=result+"Fizz";
+				}
+				else {
+					result=result+loop_input_number;
+				}
+				
+			}
+			
+		}
 		
-		
-		return "0";
+		return result;
 		
 	}
 
--- FizzBuzzKataTest.java
+++ FizzBuzzKataTest.java
@@ -5,14 +5,18 @@ import org.junit.jupiter.api.Test;
 
 class FizzBuzzKataTest {
 	FizzBuzz fizzbuzz=new FizzBuzz();
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-	@Test
-	void test1() {
+	void test_of_0() {
 		assertEquals("-1",fizzbuzz.Print_Fizz_Buzz(0));
 	}
-
+	@Test
+	void test_of_1() {
+		assertEquals("1",fizzbuzz.Print_Fizz_Buzz(1));
+	}
+	@Test
+	void test_of_3() {
+		assertEquals("12Fizz",fizzbuzz.Print_Fizz_Buzz(3));
+	}
 
 }
