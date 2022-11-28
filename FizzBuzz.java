50c314aa01b015aa59f29a43fea443a7a3087fc0
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-11-28 16:17:26 +0530

    First Testcase

---
+++ FizzBuzz.java
@@ -0,0 +1,20 @@
+
+public class FizzBuzz {
+	public String Print_Fizz_Buzz(int input_number) {
+		
+		if(input_number>100 || input_number<0) {
+			return "-1";
+		}
+		
+		
+		return "0";
+		
+	}
+
+
+
+}
+//public static void main(String[] args) {
+//	// TODO Auto-generated method stub
+//
+//}
---
+++ FizzBuzzKataTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class FizzBuzzKataTest {
+	FizzBuzz fizzbuzz=new FizzBuzz();
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test1() {
+		assertEquals("-1",fizzbuzz.Print_Fizz_Buzz(0));
+	}
+
+
+}
